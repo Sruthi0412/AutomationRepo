a2214b052f33d9927d805bbabb839b7f3adbd066
LENOVO <LENOVO@DESKTOP-BB73UPO>
2026-03-23 17:00:22 +0530

    Commit 8

--- AutomationCourse/src/test/java/seleniumbasics/HandlingWebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumbasics/HandlingWebElementCommands.java
@@ -1,7 +1,15 @@
 package seleniumbasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HandlingWebElementCommands extends Base{
 	
@@ -14,6 +22,12 @@ public class HandlingWebElementCommands extends Base{
 		WebElement showbutton = driver.findElement(By.id("button-one"));
 		System.out.println(showbutton.isDisplayed());
 		System.out.println(showbutton.isEnabled());
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showbutton)); //explicit wait
+		
+		Wait<WebDriver>fluent=new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		fluent.until(ExpectedConditions.elementToBeClickable(showbutton)); //fluent wait
 		showbutton.click();
 		
 		WebElement yourmessage = driver.findElement(By.id("message-one"));
